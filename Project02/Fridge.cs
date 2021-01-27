79b064c969594d7f6a359d63a678178601aaef53
JustBenjami <71096387+JustBenjami@users.noreply.github.com>
2021-01-26 17:10:18 -0800

    Appliance Clases, Fridge, Oven added

---
+++ Project02/Fridge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Project02
+{
+    public class Fridge : Appliance
+    {
+        public Fridge(string make) : base(make)
+        {
+        }
+
+        public override void TurnOn()
+        {
+            this.state = ApplianceState.on;
+            Console.WriteLine(this.make + " is now " + this.state + ".");
+            Console.WriteLine("Rumble Rumble, vrooooooom.");
+        }
+
+        public override void TurnOff()
+        {
+            this.state = ApplianceState.off;
+            Console.WriteLine(this.make + " is now " + this.state + ".");
+            Console.WriteLine("Rumble Rumble, oomhf");
+        }
+
+    }
+}
---
+++ Project02/Oven.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Project02
+{
+    public class Oven : Appliance
+    {
+        public Oven(string make) : base(make)
+        {
+        }
+
+        public override void TurnOn()
+        {
+            this.state = ApplianceState.on;
+            Console.WriteLine(this.make + " is now " + this.state + ".");
+            Console.WriteLine("ZiiiiingDiiiing!");
+        }
+
+        public override void TurnOff()
+        {
+            this.state = ApplianceState.off;
+            Console.WriteLine(this.make + " is now " + this.state + ".");
+            Console.WriteLine("Verrrrrmding");
+        }
+    }
+}
